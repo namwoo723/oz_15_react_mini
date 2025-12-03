da6b4fcb287d43cc2e41decdf0c89423098c32ad
김남우 <namwoo723@gmail.com>
2025-12-03 18:27:47 +0900

    Feat: Layout, NavBar 컴포넌트 생성 및 NavBar 상단 표시

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export default function NavBar () {
+  return (
+    <nav className="navbar">
+      <h1>
+        <Link to="/" >남플릭스</Link> 
+      </h1>
+      <input type="text"/>
+      <button>로그인</button>
+      <button>회원가입</button>
+    </nav>
+  )
+}
