aab7903e859934d89a66136e38e884809875f2d7
김남우 <namwoo723@gmail.com>
2025-12-04 15:24:55 +0900

    Fix: HomePage.jsx 생성 및 라우터, 컴포넌트 구조 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,19 @@
-import { useEffect, useState } from 'react'
-import movieListData from "./movieListData.json"
-import Moviecard from "./component/MovieCard"
 import './App.scss'
-import { Navigation, Pagination } from 'swiper/modules'
-import { SwiperSlide, Swiper } from 'swiper/react'
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import Layout from './component/Layout'
+import MovieDetail from './pages/MovieDetail'
 
-const imageBaseUrl = "https://image.tmdb.org/t/p/w500" 
 
 function App() {
-  const [ movies, setMovies ] = useState([])
-  
-  useEffect(() => {
-    // const fetchAPI = async () => {
-    //   const response = await fetch("https://image.tmdb.org/t/p/w500" )
-    //   const data = await response.json()
-    // }
-    setMovies(movieListData.results)
-  }, [])
-  console.log(import.meta.env.VITE_TMDB_TOKEN);
 
   return (
-    <>
-      <div className="app">
-        {/* 캐러셀 */}
-        <Swiper
-          modules={[Navigation, Pagination]}
-          spaceBetween={20} // 카드 사이 간격
-          slidesPerView={5} // 한 화면에 보이는 카드 개수
-          navigation // 좌우 화살표
-          pagination={{ clickable: true }} // 하단 점 네비게이션
-          loop={true} // 무한 루프
-        >
-          {movies.map((movie) => (
-            <SwiperSlide key={movie.id}>
-              <Moviecard 
-                title={movie.title}
-                posterUrl={imageBaseUrl + movie.poster_path}
-                rating={movie.vote_average}
-              />
-            </SwiperSlide>
-            ))}
-        </Swiper>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/details" element={<MovieDetail />} />
+      </Route>
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import MovieDetail from './pages/MovieDetail.jsx'
-import Layout from './component/Layout.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<App />} />
-        <Route path="/details" element={<MovieDetail />} />
-      </Route>
-    </Routes>
+    <App />
   </BrowserRouter>,
 )
